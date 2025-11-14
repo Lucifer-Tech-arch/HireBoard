7e16f1947dca116766000ddbad75dd2177867ff6
Krishna Goswami <www.goswamikrishna687@gmail.com>
2025-11-14 16:59:23 +0530

    added clerk auth

---
+++ Backend/src/lib/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+import {ENV} from './env.js'
+
+export const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(ENV.DB_URL)
+        console.log("Database Connected successfully",conn.connection.host);
+    } catch (error) {
+        console.log(error)
+        process.exit(1) //0 means success and 1 means failure
+    }
+}
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import {ENV} from './lib/env.js'
+import { connectDB } from "./lib/db.js";
 
 const port = ENV.PORT | 3000;
 
@@ -8,6 +9,14 @@ app.get("/",(req,res) => {
     res.status(200).json({msg: "success from api"});
 })
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+
+const startserver = async() => {
+    try {
+        await connectDB();
+        app.listen(port, () => console.log(`Server is running on port ${port}`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startserver();
